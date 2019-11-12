ad8aa1ced3b0d02b562598017dd113c9fe278247
sawyer <wusy@lmfun.cn>
2019-11-12 18:16:10 +0800

    init

--- src/main/java/com/rua/demo/controller/DemoController.java
+++ src/main/java/com/rua/demo/controller/DemoController.java
@@ -1,39 +1,59 @@
 package com.rua.demo.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-/**
- * @author One
- */
-@RestController
-@RequestMapping("/config")
-@RefreshScope
+
+@Controller
 public class DemoController {
 
-    @Value(value = "${message}")
-    private String message;
 
-    @Value(value = "${user.name}")
-    private String name;
+    @RequestMapping(value = "/list-parent", method = GET)
+    public String listParent() {
 
-    @RequestMapping(value = "/demo", method = GET)
-    public String demo() {
-        return "hello demo";
+        return "list-parent";
     }
 
-    @RequestMapping(value = "/hello", method = GET)
-    public String get() {
-        return "hello," + message;
+    @RequestMapping(value = "/list-child", method = GET)
+    public String listChild() {
+
+        return "list-child";
     }
 
-    @RequestMapping(value = "/name", method = GET)
-    public String name() {
-        return "hello, " + name + "message, " + message;
+    @RequestMapping(value = "/form-parent", method = GET)
+    public String formParent() {
+
+        return "form-parent";
+    }
+
+    @RequestMapping(value = "/form-child", method = GET)
+    public String formChild() {
+
+        return "form-child";
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/parentData", method = GET)
+    public String parentData() {
+
+        return "{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":[" +
+                "{\"project_name\":\"市一路\",\"start_date\":\"2019-11-11\",\"end_date\":\"2019-11-14\",\"balance\":\"100\",\"total_budget\":\"500\",\"tech_budget\":200,\"settle_budget\":200,\"extra_budget\":100}, " +
+                "{\"project_name\":\"市二路\",\"start_date\":\"2019-11-11\",\"end_date\":\"2019-11-22\",\"balance\":\"100\",\"total_budget\":\"500\",\"tech_budget\":200,\"settle_budget\":200,\"extra_budget\":100}," +
+                "{\"project_name\":\"市三路\",\"start_date\":\"2019-11-10\",\"end_date\":\"2019-12-12\",\"balance\":\"100\",\"total_budget\":\"500\",\"tech_budget\":200,\"settle_budget\":200,\"extra_budget\":100}   ]}";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/childData", method = GET)
+    public String childData() {
+
+        return "{\"code\":0,\"msg\":\"\",\"count\":1000,\"data\":[" +
+                "{\"project_name\": \"市一路\",\"project_type\": \"技术\",\"part_a\":\"甲方1\",\"part_b\":\"乙方1\",\"cost\":200,\"start_date\":\"2019-11-11\",\"end_date\":\"2019-11-11\",\"created_person\":\"张三\",\"checked_person\":\"李四\",\"sign_date\":\"2019-11-11\"}, " +
+                "{\"project_name\": \"市二路\",\"project_type\": \"技术\",\"part_a\":\"甲方1\",\"part_b\":\"乙方1\",\"cost\":200,\"start_date\":\"2019-11-11\",\"end_date\":\"2019-11-11\",\"created_person\":\"张三\",\"checked_person\":\"李四\",\"sign_date\":\"2019-11-11\"}, " +
+                "{\"project_name\": \"市三路\",\"project_type\": \"技术\",\"part_a\":\"甲方1\",\"part_b\":\"乙方1\",\"cost\":200,\"start_date\":\"2019-11-11\",\"end_date\":\"2019-11-11\",\"created_person\":\"张三\",\"checked_person\":\"李四\",\"sign_date\":\"2019-11-11\"}   ]}";
     }
 }
