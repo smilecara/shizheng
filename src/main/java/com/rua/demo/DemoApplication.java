6629064b688f2473b19dc6e856e6606dfd9aa4e8
sawyer <wusy@lmfun.cn>
2019-11-13 10:50:02 +0800

    updated scan router

--- src/main/java/com/rua/demo/DemoApplication.java
+++ src/main/java/com/rua/demo/DemoApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.rua")
+@SpringBootApplication(scanBasePackages = "com.rua.demo")
 @ServletComponentScan
-@MapperScan(basePackages = {"com.rua.mapper"})
+@MapperScan(basePackages = {"com.rua.demo.mapper"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
