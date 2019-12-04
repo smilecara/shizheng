9e8fc41ca4d19b85dd29d339e630385bdf03a503
sawyer <wusy@lmfun.cn>
2019-12-04 22:29:12 +0800

    temp

--- src/main/java/com/rua/demo/controller/ContractController.java
+++ src/main/java/com/rua/demo/controller/ContractController.java
@@ -1,6 +1,5 @@
 package com.rua.demo.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageInfo;
 import com.rua.demo.common.constant.BaseCode;
@@ -18,7 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -53,7 +51,7 @@ public class ContractController {
         ContractRequestDTO contractRequestDTO = FastJsonUtils.toBean(payload, ContractRequestDTO.class);
         PageInfo<ContractDTO> listContracts = contractService.listContracts(contractRequestDTO);
 
-        return ResultBuilderUtils.buildSuccess(listContracts);
+        return ResultBuilderUtils.buildSuccess(listContracts.getList());
     }
 
     @RequestMapping(value = "/updateContract", method = POST)
